92b98159af21fb3540dc6aa93a0b0e0868dd0e31
Alok Revi <aloksmind2@gmail.com>
2024-11-01 16:13:24 -0400

    Password Manager program committed

---
+++ Oct_29/PasswordManager/PasswordMain.java
@@ -0,0 +1,18 @@
+package Oct_29.PasswordManager;
+
+public class PasswordMain {
+
+    public static void main(String[] args) {
+
+        PasswordManager passwordManager = new PasswordManager();
+
+       /* passwordManager.userId = "AlokRevi";
+        passwordManager.password= "123456";*/
+
+
+        passwordManager.setPassword("12345");
+        passwordManager.setUserId("AlokRevi");
+        passwordManager.login("AlokRevi","12345");
+
+    }
+}
